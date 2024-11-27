6ad3672727387a454953f0585268ca47191d1176
fishwww-ww <2470335462@qq.com>
2024-11-27 20:37:44 +0800

    feat/init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <graphics.h>
+using namespace ege;
+
+int main()
+{
+	initgraph(640, 480);
+	setcolor(EGERGB(0xFF, 0x0, 0x0));
+	setbkcolor(WHITE);
+	circle(320, 240, 100);
+	getch();
+	closegraph();
+	return 0;
+}
